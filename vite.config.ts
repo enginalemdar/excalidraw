cf6f06a118f295b1e42a9bdfb3f29ca3ca4e5adc
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-04-05 02:13:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
-    port: 8080
+    port: 8080,
   },
   preview: {
-    port: 8080
+    port: 8080,
   }
-})
+});

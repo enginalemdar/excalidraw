df37bf4e091a6ca4e83ec3b8f9a23d02789f4f1a
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-04-04 23:01:15 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/dist/excalidraw.min.css";
+
+export default function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Excalidraw />
+    </div>
+  );
+}
